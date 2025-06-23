a93d00d86f1d9829333f370e56f567103e70650f
femy-dmng <femydomingo@proton.me>
2025-06-23 11:33:17 +0200

    updates

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,15 +1,29 @@
+import Link from "next/link";
 import styles from "./navbar.module.scss";
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <h4 className={styles.navbarTitle}>BJJ Insight</h4>
+      <Link href="/" className={styles.navbarLink}>
+        <h4 className={styles.navbarTitle}>BJJ Insight</h4>
+      </Link>
+
       <ul className={styles.navbarList}>
-        <li className={styles.navbarListElement}>Les athlètes</li>
-        <li className={styles.navbarListElement}>Ipsum</li>
-        <li className={styles.navbarListElement}>Dolor</li>
-        <li className={styles.navbarListElement}>sit</li>
-        <li className={styles.navbarListElement}>FAQ</li>
+        <Link href="Athletes" className={styles.navbarLinkList}>
+          <li className={styles.navbarListElement}>Les athlètes</li>
+        </Link>
+
+        <Link href="/" className={styles.navbarLinkList}>
+          <li className={styles.navbarListElement}>Ipsum</li>
+        </Link>
+
+        <Link href="/" className={styles.navbarLinkList}>
+          <li className={styles.navbarListElement}>Dolor</li>
+        </Link>
+
+        <Link href="/" className={styles.navbarLinkList}>
+          <li className={styles.navbarListElement}>FAQ</li>
+        </Link>
       </ul>
       <button className={styles.navbarButton}>Télécharger</button>
     </nav>
---
+++ sections/athetes/athletes.tsx
@@ -0,0 +1,7 @@
+export default function AthletesTest() {
+  return (
+    <section>
+      <div>TEST</div>
+    </section>
+  );
+}
