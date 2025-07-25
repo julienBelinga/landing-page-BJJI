99aefdb1123bb4f80d2975ccdae293cbf72173c5
Julien Belinga <julienbelinga29@gmail.com>
2025-07-25 12:54:48 +0400

    refactor: transform UnderConstruction page into Home component with loading state and multiple sections
    - Converted the UnderConstruction component to a Home component with a loading state.
    - Integrated sections for Herobanner, Benefits, Functionalities, ExampleAthlete, AthleteQuote, and CallToAction.
    - Added a LoadingScreen component to enhance user experience during data loading.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
-export default function UnderConstruction() {
+"use client";
+
+import { useState } from "react";
+import AthleteQuote from "@/sections/athleteQuote/athleteQuote";
+import Benefits from "@/sections/benefits/benefits";
+import CallToAction from "@/sections/callToAction/callToAction";
+import ExampleAthlete from "@/sections/exampleAthlete/exampleAthlete";
+import Feedbacks from "@/sections/feedbacks/feedbacks";
+import Functionalities from "@/sections/functionalities/functionalities";
+import Herobanner from "@/sections/hero-banner/herobanner";
+import LoadingScreen from "@/components/loadingScreen/loadingScreen";
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingScreen onLoadingComplete={handleLoadingComplete} />;
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center px-4">
-      <div className="max-w-md">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">
-          Page en cours de développement
-        </h1>
-        <p className="text-gray-600 mb-6">
-          Cette page n’est pas encore disponible. Nous travaillons activement pour vous offrir la meilleure expérience possible. 🚧
-        </p>
-        <p className="text-gray-500 text-sm">
-          Pour toute question ou besoin de support, contactez-nous à{" "}
-          <a
-            href="mailto:julienboinga.pro@gmail.com"
-            className="text-blue-600 underline"
-          >
-            julienbelinga.pro@gmail.com
-          </a>
-        </p>
-      </div>
-    </div>
+    <>
+      <Herobanner />
+      <Benefits />
+      <Functionalities />
+      <ExampleAthlete />
+      <AthleteQuote />
+      <CallToAction />
+    </>
   );
 }
